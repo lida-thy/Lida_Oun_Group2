f0716aa802be9322c20f3be4ee693e0ccd38d3c1
Student <student@PNCA13-14.pnc.lan>
2021-12-23 23:03:00 -0300

    shooting

--- Game_VC1.py
+++ Game_VC1.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter.constants import YES
 import winsound
 import random
 window=tk.Tk()
@@ -18,6 +19,7 @@ backGround_infront=tk.PhotoImage(file="Image/bg_creen.png")
 MOVEAMEI=20
 # myhero=''
 Hero_position=0
+enamy_position=0
 X=400
 #Player======================================================================================
 
@@ -49,10 +51,13 @@ def move_to_Up(event):
     winsound.PlaySound("Sound/move_sound.wav",winsound.SND_FILENAME|winsound.SND_ASYNC)
 #Shooting==========================================================================================
 def shoot_enamei():
-    global X,Hero_position
+    global X,Hero_position,enamy_position,score
     Y=Hero_position[1]
-    X+=5
+    X+=10
     bulete=canvas.create_image(X,Y,image=bulet_game,anchor="nw")
+    print(X,Y)
+    if enamy_position==bulete:
+        score+=1
     if X>-150:
         canvas.after(10,lambda:shoot_enamei())
     canvas.after(10,lambda:canvas.delete(bulete))
@@ -64,10 +69,12 @@ def to_shoot(event):
 
 #Anamei ===========================================================================================
 def Anamei_random():
+    global enamy_position
     canvas.delete("delete")
     x=775
     y=random.randrange(25,475)
-    canvas.create_image(x,y,image=annamei,tags="delete")
+    enamy=canvas.create_image(x,y,image=annamei,tags="delete")
+    enamy_position=canvas.coords(enamy)
     canvas.after(10000,lambda:Anamei_random())
     print(y,x)
 #Display =====================================================================
@@ -75,13 +82,13 @@ position_x=35
 position_y=400
 score=00
 def button_Click_play():
-    global myhero,gameBul,x,y,bulet,score
+    global myhero,gameBul,x,y,score,Hero_position
     winsound.PlaySound("Sound/sound back.wav",winsound.SND_FILENAME|winsound.SND_ASYNC)
     Anamei_random()
     canvas.create_image(400,250,image=background)
-    # gameBul=canvas.create_oval(40,400,50,410,fill="red")
-    canvas.create_text(70,20,text="Score is : "+str(score),font=("Purisa",20))
+    canvas.create_text(100,20,text="Score is : "+str(score)+"/100",font=("Purisa",20))
     myhero=canvas.create_image(position_x,position_y,image=Hero)
+    Hero_position=canvas.coords(myhero)
     buttonClickToPlay.place_forget()
 #Main code==================================================================
 canvas.create_image(400,250,image=backGround_infront)
@@ -91,6 +98,7 @@ window.bind("<Left>",move_to_left)
 window.bind("<Down>",move_to_Down)
 window.bind("<Up>",move_to_Up)
 window.bind("<b>",to_shoot)
+window.bind("<Button-1>",to_shoot)
 buttonClickToPlay =tk.Button(window, text="Start Game",width=20, height=3, bd="10", command=button_Click_play, background="brown")
 buttonClickToPlay.place(x=320, y=400)
 canvas.pack(expand=True,fill="both")
